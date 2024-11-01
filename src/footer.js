b5ccd8a3bd7a3ff28db8ec1939e83e2a13844fe5
Sarwar Murshed <murshem@clarkson.edu>
2024-11-01 09:30:08 -0500

    added js footer

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const footer = document.createElement("footer");
+  const horizontalLine = document.createElement("hr");
+  horizontalLine.style.marginBottom = "10px"; // optional
+  footer.style.textAlign = "center";
+  footer.style.fontWeight = "normal";
+  footer.innerHTML = `Copyright &copy; M. G. Sarwar Murshed. All rights reserved. 
+			Last modified: ${new Date().toLocaleDateString()}`;
+  document.body.appendChild(horizontalLine);
+  document.body.appendChild(footer);
+});
